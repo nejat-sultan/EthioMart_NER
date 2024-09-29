1829181272e11792000364de9466fd47df491704
Nejat Sultan <nejatdarge@gmail.com>
2024-09-29 10:31:45 +0300

    scrapping msg and preprocessing done

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,11 +1,14 @@
-from telethon.sync import TelegramClient # type: ignore
+from telethon.sync import TelegramClient  # type: ignore
 import os
-import pandas as pd # type: ignore
+import pandas as pd  # type: ignore
+import asyncio
+from dotenv import load_dotenv # type: ignore
 
+load_dotenv()
 
-api_id = '27481742'
-api_hash = '58e0a00cff4b76e4bd54d6139e0521ea'
-phone = '+251900462410'
+api_id = os.getenv('TG_API_ID')
+api_hash = os.getenv('TG_API_HASH')
+phone = os.getenv('PHONE')
 
 client = TelegramClient('session_name', api_id, api_hash)
 
@@ -14,19 +17,43 @@ async def get_messages(channel_name, limit=100):
     channel = await client.get_entity(channel_name)
     
     messages = []
-    async for message in client.iter_messages(channel, limit=limit):
-        if message.text:  
-            messages.append({'sender_id': message.sender_id, 
-                             'text': message.text, 
-                             'date': message.date})
+
+    media_dir = 'media'
+    os.makedirs(media_dir, exist_ok=True)
+
+    try:
+        async for message in client.iter_messages(channel, limit=limit):
+            message_data = {
+                'sender_id': message.sender_id,
+                'text': message.text if message.text else '',
+                'date': message.date,
+                'media_path': None
+            }
+
+            if message.media and hasattr(message.media, 'photo'):
+                filename = f"{channel_name}_{message.id}.jpg"
+                media_path = os.path.join(media_dir, filename)
+                await client.download_media(message.media, media_path)
+                message_data['media_path'] = media_path
+
+            messages.append(message_data)
+
+            await asyncio.sleep(0.5)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
     
     df = pd.DataFrame(messages)
+
     if not os.path.exists('raw_data'):
         os.makedirs('raw_data')
+        
     df.to_csv('raw_data/messages.csv', index=False) 
-
     
     return df
 
+async def main():
+    await get_messages('@Leyueqa')
+
 with client:
-    client.loop.run_until_complete(get_messages('@Leyueqa'))
+    client.loop.run_until_complete(main())
